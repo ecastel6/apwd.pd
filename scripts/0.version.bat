20eabbeb89a50b9dec9b33b9a79084a30037ac4d
jbernal <jbernal@etsisi.upm.es>
2016-09-15 23:53:55 +0200

    Refactor

--- scripts/0.version.bat
+++ scripts/0.version.bat
@@ -1,9 +1,13 @@
 @echo off
+:: Con :: se establecen comentarios
+::set se definen variables
 set workspace=C:\DatosJBB\WorkSpaces\Luna\IWVG.Maven
 ::Las variables de entorno se pueden configurar directamente en Windows cuando se tienen los permisos necesarios
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
 set M2_HOME=C:\Archivos_de_programa\apache-maven-3.3.3
+:: %var% se accede a una variable
 set PATH=%PATH%;%JAVA_HOME%\bin;%M2_HOME%\bin
+::echo saca por pantalla un mensaje
 echo -----------------------------------------
 echo . (C) MIW
 echo -----------------------------------------
@@ -15,6 +19,7 @@ echo .
 cd %workspace%
 echo ============ mvn --version =======================================================
 echo .
+:: Se ejecuta un comando maven
 call mvn --version
 echo .
 ::Se queda la consola abierta para realizar comandos en línea
--- src/test/java/es/upm/miw/iwvg/junit/AllIwvgJunittests.java
+++ src/test/java/es/upm/miw/iwvg/junit/AllIwvgJunittests.java
@@ -4,16 +4,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import es.upm.miw.iwvg.junit.solution.DecimalCollectionTest;
-import es.upm.miw.iwvg.junit.solution.FractionTest;
-import es.upm.miw.iwvg.junit.solution.UserTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({
     PointTest.class, 
-    DecimalCollectionTest.class,
-    FractionTest.class,
-    UserTest.class
+    //DecimalCollectionTest.class,
+    //FractionTest.class,
+    //UserTest.class
 })
 public class AllIwvgJunittests {
 
