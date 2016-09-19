2314416f3d9643ad106963086f671957a3ad70eb
jbernal <jbernal@etsisi.upm.es>
2016-09-19 11:30:13 +0200

    refactor script

--- scripts/0.version.bat
+++ scripts/0.version.bat
@@ -1,7 +1,7 @@
 @echo off
 :: Con :: se establecen comentarios
 ::set se definen variables
-set workspace=C:\DatosJBB\WorkSpaces\Luna\IWVG.Maven
+set workspace=C:\DatosJBB\WorkSpaces\Luna\IWVG.Ide
 ::Las variables de entorno se pueden configurar directamente en Windows cuando se tienen los permisos necesarios
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_65
 set M2_HOME=C:\Archivos_de_programa\apache-maven-3.3.3
