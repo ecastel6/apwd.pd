a93f4dfda8d016f11c115c4189b686d86a70faaf
Emilio Castellano <ecastel6@gmail.com>
2016-10-10 13:06:43 +0200

    Limpieza de repositorio

--- src/main/java/es/upm/miw/pd/composite/expression/solution/Numero.java
+++
@@ -1,8 +0,0 @@
-package es.upm.miw.pd.composite.expression.solution;
-
-public class Numero {
-	private int valor;
-	public Numero (int num){
-		this.valor=num;
-	}
-}
--- src/main/java/es/upm/miw/pd/state/connection/solution/Conexion.java
+++
@@ -1,5 +0,0 @@
-package es.upm.miw.pd.state.connection.solution;
-
-public class Conexion {
-
-}
